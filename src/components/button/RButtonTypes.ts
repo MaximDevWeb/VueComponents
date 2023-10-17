ace4c025ebcff06f1ad1afa6c68e118a633301bf
Maxim Zhukovich <maxim.dev.web@gmail.com>
2023-10-17 10:09:18 +0300

    Добавил компонент RButton

---
+++ src/components/button/RButtonTypes.ts
@@ -0,0 +1,14 @@
+export type RButtonTarget = 'button' | 'submit' | 'reset'
+
+export type RButtonType =
+  | 'primary'
+  | 'primary-light'
+  | 'secondary'
+  | 'secondary-gray'
+  | 'tertiary'
+  | 'danger'
+  | 'danger-light'
+  | 'danger-grey'
+  | 'danger-white'
+
+export type RButtonSize = 'big' | '' | 'small'
