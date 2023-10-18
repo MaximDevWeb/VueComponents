5f424c97b2364ab8ce5d52e3cc2eec8197417e1f
Maxim Zhukovich <maxim.dev.web@gmail.com>
2023-10-18 11:01:48 +0300

    Небольшие праки по StoryBook и замена outline на box-shadow

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
 import '@/assets/styles/app.scss'
-import RIcon from '@/components/icon/RIcon.vue'
-import RCheckbox from '@/components/checkbox/RCheckbox.vue'
 </script>
 
-<template>
-  <r-checkbox disable>gffhfgh</r-checkbox>
-</template>
+<template></template>
