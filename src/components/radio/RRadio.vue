35f2e14351757ea3190f712bac59493d91128134
Maxim Zhukovich <maxim.dev.web@gmail.com>
2023-10-18 23:30:27 +0300

    Добавил компонент Radio

---
+++ src/components/radio/RRadio.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import './assets/radio.scss'
+import checkIcon from './assets/check.svg'
+import { computed } from 'vue'
+
+const props = defineProps({
+  value: {
+    type: [String, Number],
+    required: true,
+    default: ''
+  },
+  disable: {
+    type: Boolean,
+    default: false
+  },
+  modelValue: {
+    type: [String, Number, Boolean]
+  }
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+/**
+ * Обрабатываем событие изменения значения
+ * @param value
+ */
+const changeHandler = (value: string | number | boolean) => {
+  emits('update:modelValue', value)
+}
+
+/**
+ * Вычисляем отмечен элемент или нет
+ */
+const isChecked = computed(() => props.value === props.modelValue)
+</script>
+
+<template>
+  <label class="r-radio">
+    <input type="radio" :checked="isChecked" :value="value" @change="changeHandler(value)" />
+
+    <span class="r-radio__field">
+      <img :src="checkIcon" alt="check" />
+    </span>
+
+    <slot />
+  </label>
+</template>
