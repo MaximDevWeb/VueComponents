61550d7bd4135237cce099161681c03653fbc3b9
Maxim Zhukovich <maxim.dev.web@gmail.com>
2023-10-25 16:31:51 +0300

    Добавлена Home страница в StoryBook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
-  stories: ['../src/components/**/*.mdx', '../src/components/**/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/components/Intro.mdx',
+    '../src/components/**/*.mdx',
+    '../src/components/**/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
