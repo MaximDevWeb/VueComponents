55284da8b0bb4c57cedfb243ce202df872bbc0a7
Maxim Zhukovich <maxim.dev.web@gmail.com>
2023-10-26 22:43:47 +0300

    Добавил компонент Pagination

---
+++ src/components/pagination/assets/left_arrow.svg.ts
@@ -0,0 +1,5 @@
+export default `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13.5,8h-11 M2.5,8L7,12.5 M2.5,8L7,3.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`
---
+++ src/components/pagination/assets/right_arrow.svg.ts
@@ -0,0 +1,5 @@
+export default `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2.5 8H13.5M13.5 8L9 3.5M13.5 8L9 12.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`
