3122119b6897c6a80f28536491526257e61faa97
Maxim Zhukovich <maxim.dev.web@gmail.com>
2023-11-01 09:43:16 +0300

    Добавил компонент Dropdown

---
+++ src/components/dropdown/RDropdownItem.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import './assets/dropdown.scss'
+</script>
+
+<template>
+  <div class="r-dropdown__item">
+    <slot />
+  </div>
+</template>
---
+++ src/components/dropdown/RDropdownTypes.ts
@@ -0,0 +1,4 @@
+export interface Height {
+  change(height: number): void
+  reset(): void
+}
