57aabe76425af763b9c5a09bea3a6e27ed7fd4df
Maxim Zhukovich <maxim.dev.web@gmail.com>
2023-11-07 18:16:10 +0300

    Добавил компонент Input и его вариации

---
+++ src/components/input/RInput.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import './assets/input.scss'
+import type { PropType } from 'vue'
+import type { RInputType } from '@/components/input/RInputTypes'
+
+defineProps({
+  type: {
+    type: String as PropType<RInputType>,
+    default: 'text'
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: String
+  },
+  disable: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+/**
+ * Обработка события ввода
+ * @param event
+ */
+const inputHandler = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emits('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <div class="r-input__wrapper">
+    <input
+      class="r-input"
+      :type="type"
+      :placeholder="placeholder"
+      :value="modelValue"
+      :disabled="disable"
+      @input="inputHandler"
+    />
+  </div>
+</template>
---
+++ src/components/input/RTextarea.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import './assets/input.scss'
+
+defineProps({
+  rows: {
+    type: Number,
+    default: 4
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: String
+  },
+  disable: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+/**
+ * Обработка события ввода
+ * @param event
+ */
+const inputHandler = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emits('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <div class="r-input__wrapper">
+    <textarea
+      class="r-textarea"
+      :placeholder="placeholder"
+      :value="modelValue"
+      :disabled="disable"
+      :rows="rows"
+      @input="inputHandler"
+    ></textarea>
+  </div>
+</template>
